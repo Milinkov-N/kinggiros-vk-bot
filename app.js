dbc92ecc818b9956d9d9d550672ee2c5e8cb2d31
Milinkov-N <milinkov.nik@gmail.com>
2022-04-24 11:46:37 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+import { VK } from 'vk-io'
+import dotenv from 'dotenv'
+import { newOrderMessage, sendBotMessage, getCartItemsStr } from './utils.js'
+
+dotenv.config()
+
+const token = process.env.VK_BOT_ACCESS_TOKEN
+const chat_id = process.env.TELEGRAM_KG_CHAT_ID
+const vk = new VK({token})
+
+const getLastOrder = async () => {
+  const data = await vk.api.market.getGroupOrders({
+    group_id: 118017840,
+    count: 1
+  })
+
+  return data.items[0]
+}
+
+vk.updates.on('market_order_new', async () => {
+  const {
+    display_order_id: orderId,
+    recipient: { name, phone },
+    delivery: { address },
+    total_price: { text: total },
+    preview_order_items: items,
+    comment
+  } = await getLastOrder()
+
+  const orderDetails = getCartItemsStr(items)
+
+  const message = newOrderMessage({
+    orderId,
+    name,
+    phone,
+    address,
+    total,
+    orderDetails,
+    comment
+  })
+
+  const tgRes = await sendBotMessage(chat_id, message)
+})
+
+vk.updates.start().catch(console.error)
---
+++ utils.js
@@ -0,0 +1,62 @@
+import fetch from 'node-fetch'
+
+export const currencyFormatter = new Intl.NumberFormat(undefined, {
+  currency: 'rub',
+  style: 'currency',
+  currencyDisplay: 'code',
+  minimumFractionDigits: 0,
+}).format
+
+export async function sendBotMessage(chat_id, text) {
+  try {
+    const res = await fetch(`https://api.telegram.org/bot${ process.env.TELEGRAM_BOT_TOKEN }/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id,
+        text
+      })
+    })
+
+    const data = await res.json()
+
+    return data.ok
+  } catch (e) {
+    console.log(e)
+    return false
+  }
+}
+
+export function newOrderMessage({
+    orderId,
+    name,
+    phone,
+    address,
+    total,
+    orderDetails,
+    comment
+}){
+  const currentDate = new Date()
+  const dateStr = currentDate.toLocaleDateString()
+  const timeStr = currentDate.toLocaleTimeString()
+
+  return `
+Новый заказ в группе King Giros!
+№${ orderId }
+${ dateStr } / ${ timeStr }
+
+ИНФОРМАЦИЯ О КЛИЕНТЕ:
+Имя - ${ name }
+Телефон - ${ phone }
+Адрес доставки - ${ address }
+Комментарий: ${ comment }
+
+ДЕТАЛИ ЗАКАЗА:
+${ orderDetails }
+ИТОГО: ${ total }
+  `
+}
+
+export const getCartItemsStr = (items => items.reduce((prevStr, item) => `${ prevStr }${ item.title } x${ item.quantity } - ${ item.price.text }\n`, ''))
